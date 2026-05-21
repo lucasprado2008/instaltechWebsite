e2503b1f4d67ce391abdd33fea80985125f6c078
Lucas <lucashenriquecardosoprado@gmail.com>
2026-05-21 17:36:52 -0300

    feat: adding header with mobile menu

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.btn-menu');
+const nav = document.querySelector('.nav');
+const overlay = document.querySelector('.overlay');
+
+function toggleMenu() {
+  nav.classList.toggle('active');
+  overlay.classList.toggle('active')
+
+  if (nav.classList.contains('active')) {
+    menuBtn.setAttribute('aria-expanded', 'true');
+  } else {
+    menuBtn.setAttribute('aria-expanded', 'false');
+  }
+}
