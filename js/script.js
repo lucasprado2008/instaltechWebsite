9d0311b01b6eaac3adb222616257c39191c16f50
Lucas <lucashenriquecardosoprado@gmail.com>
2026-06-26 11:48:22 -0300

    feat: improve website responsive layout for laptop

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
-const links = document.querySelectorAll('.nav a');
-const menuBtn = document.querySelector('.btn-menu');
-const nav = document.querySelector('.nav');
-const overlay = document.querySelector('.overlay');
+const links = document.querySelectorAll(".nav a");
+const menuBtn = document.querySelector(".btn-menu");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
 
 function toggleMenu() {
-  document.body.classList.toggle('menu-active')
-  nav.classList.toggle('active');
-  overlay.classList.toggle('active')
+  document.body.classList.toggle("menu-active");
+  nav.classList.toggle("active");
+  overlay.classList.toggle("active");
 
-  if (nav.classList.contains('active')) {
-    menuBtn.setAttribute('aria-expanded', 'true');
+  if (nav.classList.contains("active")) {
+    menuBtn.setAttribute("aria-expanded", "true");
   } else {
-    menuBtn.setAttribute('aria-expanded', 'false');
+    menuBtn.setAttribute("aria-expanded", "false");
   }
 }
 
-links.forEach(link => link.addEventListener('click', toggleMenu));
-document.querySelector('.btn-menu').addEventListener('click', toggleMenu);
-document.querySelector('.overlay').addEventListener('click', toggleMenu);
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    if (window.innerWidth < 1024) {
+      toggleMenu();
+    }
+  });
+});
+document.querySelector(".btn-menu").addEventListener("click", toggleMenu);
+document.querySelector(".overlay").addEventListener("click", toggleMenu);
